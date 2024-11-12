6c0a9a9a6d3c3479a94b2423dded184cbc6d08e0
mainaliashish <mainaliashish@outlook.com>
2024-11-12 09:12:12 +0000

    update: - add reservation reminder using reservation context

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
 import Filter from "@/app/_components/Filter";
+import ReservationReminder from "@/app/_components/ReservationReminder";
 
 /*
   -> should be some exact value in seconds
@@ -37,6 +38,7 @@ export default async function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
