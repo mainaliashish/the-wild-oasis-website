b1a7b46786241f52c02684ea3b8bb3e39c97de88
mainaliashish <mainaliashish@outlook.com>
2024-11-12 08:04:51 +0000

    update: - make current page active

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,8 +1,11 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/logo.png";
+import { usePathname } from "next/navigation";
 
 function Logo() {
+  const pathname = usePathname();
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       <Image
@@ -13,7 +16,11 @@ function Logo() {
         quality={100}
         alt="The Wild Oasis logo"
       />
-      <span className="text-xl font-semibold text-primary-100">
+      <span
+        className={`text-xl font-semibold  ${
+          pathname === "/" ? "text-accent-400" : "text-primary-100"
+        }`}
+      >
         The Wild Oasis
       </span>
     </Link>
--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,13 +1,18 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+  const pathname = usePathname();
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
         <li>
           <Link
             href="/cabins"
-            className="hover:text-accent-400 transition-colors"
+            className={`hover:text-accent-400 transition-colors ${
+              pathname === "/cabins" ? "text-accent-400" : ""
+            }`}
           >
             Cabins
           </Link>
@@ -15,7 +20,9 @@ export default function Navigation() {
         <li>
           <Link
             href="/about"
-            className="hover:text-accent-400 transition-colors"
+            className={`hover:text-accent-400 transition-colors ${
+              pathname === "/about" ? "text-accent-400" : ""
+            }`}
           >
             About
           </Link>
@@ -23,7 +30,9 @@ export default function Navigation() {
         <li>
           <Link
             href="/account"
-            className="hover:text-accent-400 transition-colors"
+            className={`hover:text-accent-400 transition-colors ${
+              pathname === "/account" ? "text-accent-400" : ""
+            }`}
           >
             Guest area
           </Link>
