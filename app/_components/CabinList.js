a6ef04e51e05cabf7c3984933f5cbeb69ab40fe9
mainaliashish <mainaliashish@outlook.com>
2024-11-12 08:08:04 +0000

    update: - cabinList component - select country js

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,9 +1,9 @@
-import React from "react";
 import CabinCard from "@/app/_components/CabinCard.js";
 import { getCabins } from "@/app/_lib/data-service";
+import { filterCabins } from "@/app/_utils/utils";
 // import { unstable_noStore as noStore } from "next/cache";
 
-export default async function CabinList() {
+export default async function CabinList({ filter }) {
   // Dynamic rendering at component level
   // Immediate effect opting out data cache
   // Manual re-validation disabling cache
@@ -11,9 +11,11 @@ export default async function CabinList() {
   const cabins = await getCabins();
   if (!cabins.length) return null;
 
+  const displayedCabins = filterCabins(filter, cabins);
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
