714409670318b3faf36ca52bf403d8432c285f1b
mainaliashish <mainaliashish@outlook.com>
2024-11-12 08:04:19 +0000

    add: - add constants

---
+++ app/_utils/constants.js
@@ -0,0 +1,22 @@
+export const filterConstants = [
+  {
+    id: 1,
+    name: "all",
+    title: "All cabins",
+  },
+  {
+    id: 2,
+    name: "small",
+    title: "1-3 guests",
+  },
+  {
+    id: 3,
+    name: "medium",
+    title: "4-7 guests",
+  },
+  {
+    id: 4,
+    name: "large",
+    title: "8-12 guests",
+  },
+];
