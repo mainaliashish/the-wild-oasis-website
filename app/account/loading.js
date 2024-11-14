9e6d3c8072c704418c95083b1b6600af8181c5ca
mainaliashish <mainaliashish@outlook.com>
2024-11-14 10:55:48 +0000

    update: - add loading page - update account and profile page

---
+++ app/account/loading.js
@@ -0,0 +1,5 @@
+import Spinner from "@/app/_components/Spinner.js";
+
+export default function Loading() {
+  return <Spinner />;
+}
--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,14 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome Ashish!
+      Welcome {session.user.name.split(" ").at(1)}!
     </h2>
   );
 }
